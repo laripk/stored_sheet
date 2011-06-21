4fc230cc8cdcc8219c3d564c924e16dd2b8e3df1
Lari Kirby <larikirby@fastmail.fm>
2011-06-21 14:44:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+What is StoredSheet?
+====================
+
+StoredSheet (or stored_sheet) is a prototype for a system that 
+lets users create tables of data as easily as they would in a spreadsheet program 
+but stores the information in a central repository.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoredSheet::Application.config.secret_token = '1efe1d3dc1c651131752afd235588ce646688e20128ec9e03f1b747bf35c3a74c72579a560e9ef518540caf8aa7637d205fe713530137df613d3e18204a703f8'
