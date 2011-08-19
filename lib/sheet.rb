605e126473d74a61066475277253eeef2cfbf85e
Lari Kirby <larikirby@fastmail.fm>
2011-08-18 22:34:29 -0400

    began model

---
+++ lib/sheet.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+require 'mongoid'
+
+
+class Sheet
+   include Mongoid::Document
+   include Mongoid::Timestamps
+   
+   field :sheet_name
+   embeds_many :columns
+   embeds_many :rows
+   
+end
+
+class Column
+   include Mongoid::Document
+   embedded_in :sheet
+   
+   
+   def self.col_name(col_num)
+      name = ""
+      num = col_num.to_i
+      raise ArgumentError, "col_num cannot be less than 1" if num < 1
+      while true do
+         num -= 1
+         n = num % 26
+         letter = ("A".ord + n).chr
+         name = "#{letter}#{name}"
+         break if num < 26
+         num = (num - n) / 26
+      end 
+      name
+   end
+   # 1  => A
+      # 1 - 1 = 0, % 26 = 0, + A = A
+   # 2  => B
+      # 2 - 1 = 1, % 26 = 1, + A = B
+   # 26 => Z
+      # 26 - 1 = 25, % 26 = 25, + A = Z
+   # 27 => AA
+      # 27 - 1 = 26, % 26 = 0, + A = A
+      # (26 - 0)/26 = 1
+      # 1 - 1 = 0, % 26 = 0, + A = A
+   # 29 => AC
+      # 29 - 1 = 28, % 26 = 2, + A = C
+      # (28 - 2)/26 = 1
+      # 1 - 1 = 0, % 26 = 0, + A = A
+   # 55 => BC
+      # 55 - 1 = 54, % 26 = 2, + A = C
+      # (54 - 2)/26 = 2
+      # 2 - 1 = 1, % 26 = 1, + A = B
+
+end
+
+class Row
+   include Mongoid::Document
+   embedded_in :sheet
+   
+end
+
--- stored_sheet.rb
+++ stored_sheet.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'mongoid'
 require 'haml'
+require './lib/sheet'
 
 configure do
    Mongoid.load!("#{settings.root}/mongoid.yml")
@@ -8,12 +9,12 @@ configure do
 end
 
 get '/' do
-  haml :index, :locals => { :title => 'Welcome to the Stored Sheet Demo!' }
+   haml :index, :locals => { :title => 'Welcome to the Stored Sheet Demo!' }
 end
 
 
 get '/shts' do
-   sheets = Sheet.only(:sheet_name, :modified).desc(:modified)
+   sheets = Sheet.only(:sheet_name, :updated_at).desc(:updated_at)
    haml :sheets, :locals => { :title => 'All Sheets', :sheets => sheets }
 end
 
