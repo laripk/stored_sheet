49f9b069eb681a7d8c313c41e035edca7235c387
Lari Kirby <larikirby@fastmail.fm>
2011-08-18 21:17:40 -0400

    home page working

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 
+gem 'haml'
+
 gem 'mongoid'
 gem 'bson_ext' # speeds up mongodb serialization
 
--- stored_sheet.rb
+++ stored_sheet.rb
@@ -1,12 +1,19 @@
 require 'sinatra'
 require 'mongoid'
+require 'haml'
 
 configure do
    Mongoid.load!("#{settings.root}/mongoid.yml")
+   set :haml, :format => :html5
 end
 
 get '/' do
-  "Hello World!"
+  haml :index, :locals => { :title => 'Welcome to the Stored Sheet Demo!' }
 end
 
+
+get '/shts' do
+   sheets = Sheet.only(:sheet_name, :modified).desc(:modified)
+   haml :sheets, :locals => { :title => 'All Sheets', :sheets => sheets }
+end
 
